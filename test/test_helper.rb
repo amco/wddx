24a10605320d22502a1358298f621d59b5c1279c
Stefan Saasen <s@juretta.com>
2008-05-01 14:19:42 +1200

    WDDX test cases working using Ruby 1.9

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,10 @@ require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/wddx'                                                   
 FIXTURES = File.dirname(__FILE__) + "/fixtures"         
 
+def is19?
+  RUBY_VERSION >= "1.9.0"
+end
+
 def read_fixture(f)
   File.read(File.join(FIXTURES, f)).gsub(/\t|\n/, "").gsub(/^(.*)<wddx/, "<wddx").gsub(/"/, "'")
 end
